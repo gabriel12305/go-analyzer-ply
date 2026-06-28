3dff36d90a7481daab446cb03bf0ffdefb20e053
gabriel12305 <gpelaezavila@gmail.com>
2026-06-28 16:36:22 -0500

    Simplify logger and remove duplicated lexical error logging - Gabriel Pelaez

--- logger.py
+++ logger.py
@@ -4,6 +4,7 @@ from lexer import lexical_errors
 from parser import syntax_errors
 from semantic import semantic_errors
 
+
 def generate_log_filename(author_name="MilenaPazmiño"):
     os.makedirs("logs", exist_ok=True)
 
@@ -25,14 +26,8 @@ def log_tokens(lexer, data, log_filename):
 
             log.write(line)
             print(line, end="")
-        
-        if lexical_errors:
-            log.write("\n===== LEXICAL ERRORS =====\n")
 
-            for error in lexical_errors:
-                log.write(error + "\n")
-    
-            
+
 def log_syntax_results(log_filename):
 
     with open(log_filename, "a", encoding="utf-8") as log:
@@ -53,20 +48,28 @@ def log_syntax_results(log_filename):
             for error in syntax_errors:
                 log.write(error + "\n")
 
+
 def generate_semantic_log_filename(author_name="CarlaGutierrez"):
     os.makedirs("logs", exist_ok=True)
+
     timestamp = datetime.now().strftime("%d-%m-%Y-%Hh%M")
     return f"logs/semantico-{author_name}-{timestamp}.txt"
 
+
 def log_semantic_errors(author_name):
-    if semantic_errors:
-        filename = generate_semantic_log_filename(author_name)
-        with open(filename, "w", encoding="utf-8") as f:
+
+    filename = generate_semantic_log_filename(author_name)
+
+    with open(filename, "w", encoding="utf-8") as f:
+
+        if semantic_errors:
             f.write("===== ERRORES SEMÁNTICOS =====\n")
+
             for error in semantic_errors:
                 f.write(error + "\n")
-        print(f"Errores semánticos guardados en {filename}")
-    else:
-        filename = generate_semantic_log_filename(author_name)
-        with open(filename, "w", encoding="utf-8") as f:
+
+            print(f"Errores semánticos guardados en {filename}")
+
+        else:
             f.write("No se encontraron errores semánticos.\n")
+            print(f"No se encontraron errores semánticos. Log generado en {filename}")
